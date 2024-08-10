64d266b46bc0f476da7fbce23d8c63634eaca293
meek <nebulameek@gmail.com>
2024-08-10 17:58:02 +0800

    init

---
+++ src/views/course/audio.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+<div>这是音频</div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
---
+++ src/views/course/video.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+<div>这是视频</div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
